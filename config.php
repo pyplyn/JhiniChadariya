3d62b63c57ff35cbb31e82e9bbf2563a97f66bd1
pyplyn <soham@pyplyn.co>
2017-07-17 20:02:11 +0530

    Initial Commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://jhinichadariya.pyplyn.in/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://jhinichadariya.pyplyn.in/');
+
+// DIR
+define('DIR_APPLICATION', '/home/pyplyin/jhiniChadariya.pyplyn.in/catalog/');
+define('DIR_SYSTEM', '/home/pyplyin/jhiniChadariya.pyplyn.in/system/');
+define('DIR_IMAGE', '/home/pyplyin/jhiniChadariya.pyplyn.in/image/');
+define('DIR_STORAGE', '/home/pyplyin/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'pyplyin_admin2');
+define('DB_PASSWORD', 'P@55word');
+define('DB_DATABASE', 'pyplyin_JC');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ system/storage/cache/ef/ef1d65029bea725cb677f436b2082c932cb792f46caa88d786dd5463b091b74d.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_e90aed4d09db8787c56d3671cec76196f0268d85681d11b5e8ce731df3b2793d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
